7bdd74d51aec928cc4d116b2ad8598de6308d648
CrypticGnome <108616755+CrypticGnome@users.noreply.github.com>
2024-07-12 20:27:41 +0100

    Deleted unused SOs

--- MARDEK Engine/Assets/Scripts/MARDEK/Inventory/Item/EquipmentCategory.cs
+++ MARDEK Engine/Assets/Scripts/MARDEK/Inventory/Item/EquipmentCategory.cs
@@ -6,14 +6,22 @@ namespace MARDEK.Inventory
     [CreateAssetMenu(menuName = "MARDEK/Inventory/EquipmentCategory")]
     public class EquipmentCategory : ScriptableObject
     {
-        [SerializeField] EquipmentSlot _slot;
+        [SerializeField] EquipmentSlotType _slot;
         [SerializeField] ScriptableColor _color;
         [SerializeField] string _classification;
 
-        public EquipmentSlot slot { get { return _slot; } }
+        public EquipmentSlotType slot { get { return _slot; } }
 
         public ScriptableColor color { get { return _color; } }
 
         public string classification { get { return _classification; } }
+          public enum EquipmentSlotType
+          {
+               MainHand,
+               OffHand,
+               Head,
+               Body,
+               Accessory
+          }
     }
 }
