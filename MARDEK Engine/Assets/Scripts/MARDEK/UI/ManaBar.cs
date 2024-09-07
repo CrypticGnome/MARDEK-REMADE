c8701f95010a97024416a81290967c0730b3ed42
CrypticGnome <108616755+CrypticGnome@users.noreply.github.com>
2024-09-07 18:31:36 +0100

    UI Improvements

---
+++ MARDEK Engine/Assets/Scripts/MARDEK/UI/ManaBar.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MARDEK.Stats;
+using MARDEK.CharacterSystem;
+using MARDEK.Battle;
+
+namespace MARDEK.UI
+{
+    public class ManaBar : MonoBehaviour
+    {
+          [SerializeField] CharacterUI characterUI = null;
+          [SerializeField] RectTransform barTransform;
+          [SerializeField] TMPro.TMP_Text statText;
+
+          private void Awake()
+          {
+               characterUI.OnInitialisation += Initialise;
+          }
+          void Initialise()
+          {
+               if (characterUI.character is null)
+                    return;
+               characterUI.character.OnStatChanged += UpdateBar;
+               UpdateBar();
+          }
+          private void Update()
+          {
+               // Currently when a stat changes the OnStatChanged delegate isn't fired. Therefore, the healthbar doesn't update.
+               // Instead of fixing that I'm just doing a quick band aid fix. Future work.
+               UpdateBar();
+          }
+          [ContextMenu("Update Bar")]
+          void UpdateBar()
+          {
+               if (characterUI.character == null)
+                    return;
+               BattleCharacter character = characterUI.character;
+               var statValue = (float)character.CurrentMP;
+               var maxStatValue = (float)character.MaxMP;
+               if (statText)
+                    statText.text = statValue.ToString();
+               if (barTransform)
+               {
+                    float xScale = Mathf.Clamp(statValue / maxStatValue , 0f, 1f);
+                    if(float.IsFinite(xScale))
+                         barTransform.localScale = new Vector3(xScale, 1, 1);
+               }
+          }
+    }
+}
