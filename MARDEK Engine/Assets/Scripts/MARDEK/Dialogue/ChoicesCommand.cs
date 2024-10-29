9a7b802ce2b4cefc4b3c5aa91fda5092e7e55441
CrypticGnome <108616755+CrypticGnome@users.noreply.github.com>
2024-10-29 13:12:02 +0000

    Choices commands working partially

--- MARDEK Engine/Assets/Scripts/MARDEK/Dialogue/ChoicesCommand.cs
+++ MARDEK Engine/Assets/Scripts/MARDEK/Dialogue/ChoicesCommand.cs
@@ -1,33 +1,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using MARDEK.Event;
+using MARDEK.Core;
+using System.Collections;
+using System;
 
 namespace MARDEK.DialogueSystem
 {
-    public class ChoicesCommand : BranchCommand
-    {
+    public class ChoicesCommand : Command
+     {
         [SerializeField] Dialogue choicesDialogue = null;
-        [SerializeField] List<CommandQueue> commandsByChoice = new List<CommandQueue>();
+          [SerializeField] CommandChain[] commandsByChoice;
 
-        public override void Trigger()
-        {
-            commandsBeingExecuted = null;
-            ChoicesManager.TriggerChoices(choicesDialogue);
-        }
+          public bool IsOngoing { get; private set; } = false;
 
-        public override bool IsOngoing()
-        {
-            if(commandsBeingExecuted == null)
-            {
-                int index = ChoicesManager.GetChosenIndex();
-                if(index > -1)
-                {
-                    commandsBeingExecuted = commandsByChoice[index];
-                    commandsBeingExecuted.TriggerFirst();
-                }
-                return true;
-            }
-            return base.IsOngoing();
-        }
-    }
+
+          [ContextMenu("Trigger")]
+          public override void Trigger()
+          {
+               if (IsOngoing)
+               {
+                    Debug.LogWarning("Trying to trigger event, but this event is already ongoing");
+                    return;
+               }
+               ChoicesManager.TriggerChoices(choicesDialogue);
+               StartCoroutine(WaitForChoice());
+
+          }
+          IEnumerator WaitForChoice()
+          {
+               // Currently, the choosing is done through the choice manager and we wait until it gives a value not equal to the default.
+               // This is dogshit, but currently no reason to rework it.
+               int index = -1;
+               yield return new WaitUntil(() =>
+               {
+                    index = ChoicesManager.GetChosenIndex();
+                    return index != -1;
+               });
+
+               if (index >= commandsByChoice.Length)
+                    throw new IndexOutOfRangeException();
+               CommandChain commands = commandsByChoice[index];
+               commands.Trigger();
+          }
+     }
 }
