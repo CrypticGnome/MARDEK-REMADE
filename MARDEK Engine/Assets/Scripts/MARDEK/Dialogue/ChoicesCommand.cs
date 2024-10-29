85067f5942771498918dd54edd8ca64a56ce14ce
CrypticGnome <108616755+CrypticGnome@users.noreply.github.com>
2024-10-29 13:34:36 +0000

    Bob

--- MARDEK Engine/Assets/Scripts/MARDEK/Dialogue/ChoicesCommand.cs
+++ MARDEK Engine/Assets/Scripts/MARDEK/Dialogue/ChoicesCommand.cs
@@ -7,18 +7,22 @@ using System;
 
 namespace MARDEK.DialogueSystem
 {
-    public class ChoicesCommand : Command
+    public class ChoicesCommand : OngoingCommand
      {
         [SerializeField] Dialogue choicesDialogue = null;
           [SerializeField] CommandChain[] commandsByChoice;
 
-          public bool IsOngoing { get; private set; } = false;
+          bool isOngoing = false;
 
+          public override bool IsOngoing()
+          {
+               return isOngoing;
+          }
 
           [ContextMenu("Trigger")]
           public override void Trigger()
           {
-               if (IsOngoing)
+               if (isOngoing)
                {
                     Debug.LogWarning("Trying to trigger event, but this event is already ongoing");
                     return;
@@ -32,11 +36,13 @@ namespace MARDEK.DialogueSystem
                // Currently, the choosing is done through the choice manager and we wait until it gives a value not equal to the default.
                // This is dogshit, but currently no reason to rework it.
                int index = -1;
+               isOngoing = true;
                yield return new WaitUntil(() =>
                {
                     index = ChoicesManager.GetChosenIndex();
                     return index != -1;
                });
+               isOngoing = false;
 
                if (index >= commandsByChoice.Length)
                     throw new IndexOutOfRangeException();
