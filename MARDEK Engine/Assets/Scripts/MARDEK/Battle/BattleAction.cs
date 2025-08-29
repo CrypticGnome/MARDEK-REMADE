895c32e945cbfe620a641bedd0b9a8d712d0c4c5
CrypticGnome <108616755+CrypticGnome@users.noreply.github.com>
2025-08-29 22:19:39 +0100

    Imported sound effects

--- MARDEK Engine/Assets/Scripts/MARDEK/Battle/BattleAction.cs
+++ MARDEK Engine/Assets/Scripts/MARDEK/Battle/BattleAction.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace MARDEK.Battle
 {
+     using MARDEK.Audio;
      using MARDEK.CharacterSystem;
      using MARDEK.Stats;
      using System;
@@ -10,12 +11,14 @@ namespace MARDEK.Battle
      public class BattleAction
      {
           [SerializeField] Element element;
+          [SerializeField] SoundEffect soundEffect;
           [SerializeReference, SubclassSelector] ActionEffects[] actionEffects;
           public Element Element { get { return element; } }
           public void Apply(BattleCharacter user, BattleCharacter target)
           {
                for (int index = 0; index < actionEffects.Length; index++)
                     actionEffects[index].ApplyEffect(user, target, element);
+               AudioManager.PlaySoundEffect(soundEffect);
           }
 
   
